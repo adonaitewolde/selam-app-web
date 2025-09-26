07138fda4ec5d9683fc0cfe653ccc60bd1c31bd7
Adonai Tewolde <adonai.tewolde@htwg-konstanz.de>
2025-09-26 22:11:47 +0200

    deleted use

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ import Link from "next/link";
 // ];
 
 export default function Example() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
 
   return (
     <div className="bg-black">
